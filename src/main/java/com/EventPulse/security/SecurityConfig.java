1bd3d729e8a4b842c57df20d3da34e2838687f6b
Yooussefff <youseffff925@gmail.com>
2025-11-18 19:45:49 +0200

    Inject and register JWTValidatorFilter; set session management to STATELESS in SecurityConfig

--- src/main/java/com/EventPulse/security/SecurityConfig.java
+++ src/main/java/com/EventPulse/security/SecurityConfig.java
@@ -1,15 +1,20 @@
 package com.EventPulse.security;
 
+import com.EventPulse.security.filter.JWTValidatorFilter;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@AllArgsConstructor
 public class SecurityConfig {
-
+    private JWTValidatorFilter jwtValidatorFilter;
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -17,15 +22,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/auth/**")
-                )
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
-                        .anyRequest().authenticated()
-                );
-
+        http.csrf(csrf -> csrf.ignoringRequestMatchers("/auth/**"))
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/**").permitAll().anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtValidatorFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
