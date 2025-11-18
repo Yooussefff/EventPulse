8bd3c530b04944c6f5101015df946361e2f23f37
Yooussefff <youseffff925@gmail.com>
2025-11-18 21:10:11 +0200

    Remove extra blank line before @RestController in AuthController.java

--- src/main/java/com/EventPulse/controller/AuthController.java
+++ src/main/java/com/EventPulse/controller/AuthController.java
@@ -12,7 +12,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @RequestMapping("/auth")
 @AllArgsConstructor
