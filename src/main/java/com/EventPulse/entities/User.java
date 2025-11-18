78e2ff8d7ddf81710c549b1842a37ee556cd1672
Yooussefff <youseffff925@gmail.com>
2025-11-18 21:09:13 +0200

    Add validation annotations to User, enforce password length, enable Lombok toBuilder, and add validation dependency

--- src/main/java/com/EventPulse/entities/User.java
+++ src/main/java/com/EventPulse/entities/User.java
@@ -2,13 +2,15 @@ package com.EventPulse.entities;
 
 import com.EventPulse.entities.enums.Role;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
-@Builder
+@Builder(toBuilder = true)
 @AllArgsConstructor
 @NoArgsConstructor
 public class User extends BaseEntity{
@@ -17,14 +19,21 @@ public class User extends BaseEntity{
     private String name;
 
     @Column(nullable = false, unique = true)
+    @Email
+    @NotBlank
     private String email;
 
-    @Column(nullable = false)
+
+    @Column(nullable = false, length = 100)
     private String password;
 
+
     @Column(nullable = false, unique = true)
+    @NotBlank
     private String userName;
 
+
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Role role;
 }
